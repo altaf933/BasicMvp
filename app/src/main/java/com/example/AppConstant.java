3036b386d2589432844bf4f731b6566ef9aca661
SA-LAP-0058\altaf.h.shaikh <altaf.h.shaikh@solutionanalysts.com>
2016-12-01 12:05:33 +0530

    File initialized for mvp API

---
+++ app/src/main/java/com/example/AppConstant.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by altaf.h.shaikh on 12/1/2016.
+ */
+
+public class AppConstant {
+//    https://holidayapi.com/v1/holidays?key=9761d9f7-0dc1-40d2-aeb4-f9544c09031e&country=in&year=2015
+    public static final String BASEURL="https://holidayapi.com/v1/holidays";
+    public static final String SECRECT_KEY="9761d9f7-0dc1-40d2-aeb4-f9544c09031e";
+}
